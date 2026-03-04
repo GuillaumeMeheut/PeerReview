29dda64f52e7835c824a65ec11334a3474121747
Guillaume Meheut <guillaume.meheutpro@gmail.com>
2026-03-04 23:03:37 +0100

    add http stripe because of runtime

--- apps/web/lib/stripe.ts
+++ apps/web/lib/stripe.ts
@@ -7,6 +7,7 @@ export function getStripe(): Stripe {
         _stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
             apiVersion: "2026-02-25.clover",
             typescript: true,
+            httpClient: Stripe.createFetchHttpClient(),
         });
     }
     return _stripe;
